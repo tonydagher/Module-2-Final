776585a5967daf0f17fc2c45c705b30391fc2f2b
Tony Dagher <tydagher@gmail.com>
2021-07-14 20:28:20 -0400

    Initial Commit

---
+++ Shared/Module_2_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module_2_FinalApp.swift
+//  Shared
+//
+//  Created by Tanios Dagher on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Module_2_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
